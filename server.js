64d887c2852a777edf6048da997cac10495289ce
Batuhan ELMACI <Batuhan.Elmac@kmkfab.local>
2026-05-15 11:49:51 +0300

    ilk dashboard arayuzu

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const db = mysql.createConnection({
     host: "veraura-db-veraura.h.aivencloud.com",
